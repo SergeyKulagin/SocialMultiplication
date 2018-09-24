6757960c3af32fe9c7d9a5143a77f8885a350322
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-09-24 15:44:41 +0300

    [Learn Microservices with Spring Boot]: - continuation with the frontend

--- src/main/resources/static/js/multiplication.js
+++ src/main/resources/static/js/multiplication.js
@@ -1,14 +1,39 @@
 $(document).ready(function () {
+    var currentMultiplication;
+
     function getMultiplication() {
         $.ajax({
             url: "/multiplications/random",
         }).then(
             function (data) {
+                currentMultiplication = data;
                 $("#multiplication-1").text(data.a);
                 $("#multiplication-2").text(data.b);
             }
         )
     }
 
+    function sendMultiplicationResult() {
+        $.ajax({
+            url: "/results",
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({
+                multiplication: currentMultiplication,
+                attemptResult: $("#result-input").val()
+            }),
+            success: function (result) {
+                if(result.correct){
+                    $("#attempt-result-message").text("Success!")
+                }else {
+                    $("#attempt-result-message").text("Failure!")
+                }
+            }
+        })
+    }
+
     getMultiplication();
+    $("#check-button").on("click", function () {
+        sendMultiplicationResult();
+    })
 })
