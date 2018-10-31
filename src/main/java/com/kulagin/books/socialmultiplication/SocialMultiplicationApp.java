4b60613e116f75a35aa4ab486856e8b734166628
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-10-31 18:12:28 +0300

    [Multiplication MS]: enable discovery client

--- src/main/java/com/kulagin/books/socialmultiplication/SocialMultiplicationApp.java
+++ src/main/java/com/kulagin/books/socialmultiplication/SocialMultiplicationApp.java
@@ -2,8 +2,10 @@ package com.kulagin.books.socialmultiplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SocialMultiplicationApp {
   public static void main(String[] args) {
     SpringApplication.run(SocialMultiplicationApp.class, args);
