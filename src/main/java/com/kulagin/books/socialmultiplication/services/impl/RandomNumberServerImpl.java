e4a8ea4e985acc7e92e2828d3db4cecf5b62f0df
sergey.kulagin <sergey.kulagin@helmes.ee>
2018-11-14 15:25:00 +0300

    [Multiplication MS]: - use random values in service - shared db service

--- src/main/java/com/kulagin/books/socialmultiplication/services/impl/RandomNumberServerImpl.java
+++ src/main/java/com/kulagin/books/socialmultiplication/services/impl/RandomNumberServerImpl.java
@@ -1,12 +1,19 @@
 package com.kulagin.books.socialmultiplication.services.impl;
 
 import com.kulagin.books.socialmultiplication.services.RandomNumberService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class RandomNumberServerImpl implements RandomNumberService{
+  private final Random random = new Random();
+  @Value("${service.random.up}")
+  private Integer randomUpBound;
+
   @Override
   public int getRandomInt() {
-    return 5;
+    return random.nextInt(randomUpBound) + 1;
   }
 }
